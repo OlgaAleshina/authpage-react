f603cc50d6110a612ae6b556a391bc52a02845aa
user <ovg43@yandex.ru>
2020-10-29 16:38:44 +0300

    refactor routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import routes from "./config/routes.js";
 import { useSelector } from "react-redux";
+import PrivateRoute from "./config/privateRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
@@ -18,16 +22,19 @@ function App() {
   return (
     <Container className="text-center">
 
-      <Switch>{routes.map(route => <Route
-        path={route.path}
-        exact={route.exact}
-        render={(props) =>
-          route.redirect ? <Redirect to={route.redirect} /> :
-            route.protected ? (
-              hasToken || isAuth ? route.component &&
-                <route.component {...props} routes={route.routes} /> : <Redirect to='/login' />
-            ) : route.component && <route.component {...props} routes={route.routes} />
-        } />)}
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <PrivateRoute exact path="/home">
+          <Home />
+        </PrivateRoute>}
       </Switch>
 
 
@@ -55,5 +62,15 @@ export default App;
   </Switch>
 
 
-
+<Switch>{routes.map(route => <Route
+        path={route.path}
+        exact={route.exact}
+        render={(props) =>
+          route.redirect ? <Redirect to={route.redirect} /> :
+            route.protected ? (
+              hasToken || isAuth ? route.component &&
+                <route.component {...props} routes={route.routes} /> : <Redirect to='/login' />
+            ) : route.component && <route.component {...props} routes={route.routes} />
+        } />)}
+      </Switch>
       */
--- src/config/privateRoute.js
+++ src/config/privateRoute.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 
 function PrivateRoute({ children, ...rest }) {
   const isAuth = useSelector(state => state.user.isAuthentificated);
-  console.log(isAuth)
-  //const isAuthenticated = false;
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    localStorage.getItem('token') ? setHasToken(true) : setHasToken(false)
+  }, []);
+
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isAuth ? (
+        isAuth || hasToken ? (
           children
         ) : (
             <Redirect
