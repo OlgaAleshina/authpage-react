17e3801dc2ebb2eee190f652b62d336e3ce64d5d
user <ovg43@yandex.ru>
2020-11-02 12:38:13 +0300

    Add error 400 component

---
+++ src/components/errorAlert.jsx
@@ -0,0 +1,10 @@
+import Alert from 'react-bootstrap/Alert'
+
+export const ErrorAlert = (error) => {
+
+    return (
+        <Alert variant="danger">
+            Error 400
+        </Alert>
+    )
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
@@ -5,6 +6,7 @@ import Form from 'react-bootstrap/Form';
 import { ErrorMessage } from '@hookform/error-message';
 import Button from 'react-bootstrap/Button';
 import { loginRequest } from '../API.js';
+import { ErrorAlert } from "../components/errorAlert";
 
 
 export const Login = () => {
@@ -12,6 +14,7 @@ export const Login = () => {
 
   const history = useHistory();
   const dispatch = useDispatch();
+  const [fetchError, setFetchError] = useState(false);
 
 
   const handleLogin = async (user) => {
@@ -22,7 +25,10 @@ export const Login = () => {
       dispatch({ type: "SET_IS_AUTH", token: response.data.token });
       history.push("/home")
     }
-    catch (err) { console.log(err); }
+    catch (err) {
+      setFetchError(true);
+      console.log(err);
+    }
 
   };
 
@@ -39,42 +45,43 @@ export const Login = () => {
   };
 
   return (
+    <>
+      {fetchError && <ErrorAlert />}
+      <Form className="container col-6 p-5" onSubmit={handleSubmit(handleLogin)}>
+        <Form.Group controlId="email"  >
+          <Form.Control type="text" placeholder="Enter email" name="email" ref={register(emailRules)} />
+          <ErrorMessage errors={errors} name="email">
+            {({ messages }) =>
+              messages &&
+              Object.entries(messages).map(([type, message]) => (
+                <p key={type}>{message}</p>
+              ))
+            }
+          </ErrorMessage>
+        </Form.Group>
 
-    <Form className="container col-6 p-5" onSubmit={handleSubmit(handleLogin)}>
-      <Form.Group controlId="email"  >
-        <Form.Control type="text" placeholder="Enter email" name="email" ref={register(emailRules)} />
-        <ErrorMessage errors={errors} name="email">
-          {({ messages }) =>
-            messages &&
-            Object.entries(messages).map(([type, message]) => (
-              <p key={type}>{message}</p>
-            ))
-          }
-        </ErrorMessage>
-      </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Control type="password" placeholder="Password" name="password" ref={register({ required: true })} />
 
-      <Form.Group controlId="password">
-        <Form.Control type="password" placeholder="Password" name="password" ref={register({ required: true })} />
+        </Form.Group>
 
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Login
+        <Button variant="primary" type="submit">
+          Login
       </Button>
-      <Button variant="light" onClick={handleRegister}>
-        Register
+        <Button variant="light" onClick={handleRegister}>
+          Register
         </Button>
-    </Form>
-
+      </Form>
+    </>
   )
 };
 
 /*loginRequest(user)
-      .then(res => { localStorage.setItem("token", res) })
-      .then(() => dispatch({ type: "SET_IS_AUTH" }))
-      .then(() => history.push("/home"))
-      .catch(err => {
-        console.log(err);
-      });
+  .then(res => {localStorage.setItem("token", res)})
+  .then(() => dispatch({type: "SET_IS_AUTH" }))
+  .then(() => history.push("/home"))
+  .catch(err => {
+    console.log(err);
+});
 
- */
+*/
